436c8fcdcf1894a4943f263d22acd86f93c7a767
haimgoldfisher <haimngoldfisher@gmail.com>
2021-11-13 21:25:20 +0200

    main

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "my_mat.h"
+
+int main()
+{
+    char functionName;
+    scanf("%c", &functionName);
+    while (functionName != 'D')
+    {
+        if (functionName == 'A')
+        {
+            /* code */
+        }
+        if (functionName == 'B')
+        {
+            /* code */
+        }
+        if (functionName == 'C')
+        {
+            /* code */
+        }
+    }
+}
